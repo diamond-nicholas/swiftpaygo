5e4a2942318483069e0ca9f2ecb4a801c8066e5b
diamond nicholas <diamondnicholas154@gmail.com>
2024-02-23 13:45:54 +0000

    add utils

---
+++ src/utils/randomString.js
@@ -0,0 +1,10 @@
+const randomString = (length) => {
+  const chars =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var result = "";
+  for (var i = length; i > 0; --i)
+    result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+};
+
+module.exports = randomString;
